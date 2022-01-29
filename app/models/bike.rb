f56d7379f3af441f5a85ee3ad8c1dbe74b78ff69
sophie mattacks <s.mattacks@hotmail.co.uk>
2022-01-29 15:20:07 +0000

    Models Complete

--- app/models/bike.rb
+++ app/models/bike.rb
@@ -1,3 +1,5 @@
 class Bike < ApplicationRecord
+  validates :title, :description, :type, :location, :cost, presence: true
+  validates :description, inclusion: { in: 50..150 }
   belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  validates :username, :email, presence: true
+  has_many :bookings
+  has_many :bikes
 end
