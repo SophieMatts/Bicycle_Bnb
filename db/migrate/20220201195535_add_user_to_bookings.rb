2dbaa2f7649b78897b8d2395ed8be7f2acc23f7f
Anton Gordon <anton.gordon85@outlook.com>
2022-02-01 19:57:02 +0000

    user added to bookings

---
+++ db/migrate/20220201195535_add_user_to_bookings.rb
@@ -0,0 +1,5 @@
+class AddUserToBookings < ActiveRecord::Migration[7.0]
+  def change
+    add_reference :bookings, :user, index: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_29_171402) do
+ActiveRecord::Schema.define(version: 2022_02_01_195535) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,8 @@ ActiveRecord::Schema.define(version: 2022_01_29_171402) do
     t.string "title"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_bikes_on_user_id"
   end
 
   create_table "bookings", force: :cascade do |t|
@@ -31,7 +33,9 @@ ActiveRecord::Schema.define(version: 2022_01_29_171402) do
     t.bigint "bike_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id"
     t.index ["bike_id"], name: "index_bookings_on_bike_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
