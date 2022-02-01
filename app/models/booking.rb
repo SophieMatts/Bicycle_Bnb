742bdcdbbc97c95276facbe50108c65d0fa7d92d
Arham Zubair <arhamzubair00@gmail.com>
2022-02-01 20:27:47 +0000

    Controllers for Bikes and Bookings

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,7 +2,7 @@ class Booking < ApplicationRecord
   validates :start_date, :end_date, presence: true, availability: true
   validate :end_date_after_start_date
   belongs_to :user
-  belongs_to :bike
+  belongs_to :bike_id
 
   private
 
