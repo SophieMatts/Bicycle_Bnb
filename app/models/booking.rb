6fa5a46edfed5812b0dc6983d1855cc0848eaf16
minsoo cho <khg04170@gmail.com>
2022-02-04 09:16:50 +0900

    Add association to user,booking model

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,7 +2,7 @@ class Booking < ApplicationRecord
   validates :start_date, :end_date, presence: true
   validate :end_date_after_start_date
 
-  belongs_to :bike
+  belongs_to :bike, :user
   private
 
   def end_date_after_start_date
