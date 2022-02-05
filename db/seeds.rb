8459df5aca70dcb1fc14f795c0a33df92416c52d
Anton Gordon <anton.gordon85@outlook.com>
2022-02-05 12:03:30 +0000

    working on booking card

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,17 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require "faker"
 
 types_of_bikes = ['Road Bike', 'Mountain Bike', 'Touring Bike', 'Folding Bike', 'BMX']
 descriptions = ['Excellent for cruising', 'Great for getting from A - B quickly', 'Just a fun bike', 'Collectors Item']
+locations = ['London', 'Manchester', 'Birmingham', 'Leeds', 'Glasgow', 'Southampton', 'Liverpool', 'Newcastle', 'Sheffield', 'Belfast']
 
 10.times do
   bike = Bike.new(
-    title: 'Bike for rent',
+    title: "#{Faker::Space.nasa_space_craft} #{(rand(1..9) * 1000)}",
     description: descriptions.sample,
     bike_type: types_of_bikes.sample,
-    location: 'Coventry',
-    cost: 233,
+    location: locations.sample,
+    cost: rand(20..200)
   )
 
   if bike.save
@@ -24,6 +26,26 @@ descriptions = ['Excellent for cruising', 'Great for getting from A - B quickly'
     puts "Failed"
   end
 
+  user = User.create! :email => Faker::Internet.email, :password => 'topsecret', :password_confirmation => 'topsecret'
+  if user.save
+    puts "User added"
+  else
+    puts "Failed"
+  end
+
+  booking = Booking.create(
+    user_id: user.id,
+    bike_id: bike.id,
+    start_date: Date.today,
+    end_date: Date.today
+  )
+   if booking.save
+    puts "Booking added"
+  else
+    puts "Failed"
+  end
 end
+
+
 
 puts "Seeds added....."
