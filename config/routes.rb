636785dc43ae8301f182887293fdc67ccd148b8a
sophie mattacks <s.mattacks@hotmail.co.uk>
2022-02-05 10:08:34 +0000

    Cards and Footer

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root 'bikes#index'
-  # root 'pages#home'
+  # root 'bikes#index'
+  root 'pages#home'
 
 
 end
